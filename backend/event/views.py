049869ce4132f891278935b8b6000c40a4937b34
Josie Fessey <07jreckn@gmail.com>
2024-07-17 18:58:00 +0300

    first commit

---
+++ backend/event/views.py
@@ -0,0 +1,51 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.dateparse import parse_date
+import json
+
+from recipient.models import Recipient
+from .models import Event
+
+from project import settings
+
+
+@csrf_exempt
+def create_event(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+
+        date = parse_date(data.get('date'))
+        bouquet_makers_needed = data.get('bouquet_makers_needed')
+        drivers_needed = data.get('drivers_needed')
+        group = data.get('group')
+        closed = data.get('closed', False)
+
+
+        event = Event.objects.create(
+            date=date,
+            bouquet_makers_needed=bouquet_makers_needed,
+            drivers_needed=drivers_needed,
+            group=group,
+            closed=closed
+        )
+
+
+        bouquet_makers_ids = data.get('bouquet_makers', [])
+        bouquet_makers = settings.AUTH_USER_MODEL.objects.filter(id__in=bouquet_makers_ids)
+        event.bouquet_makers.add(*bouquet_makers)
+
+
+        drivers_ids = data.get('drivers', [])
+        drivers = settings.AUTH_USER_MODEL.objects.filter(id__in=drivers_ids)
+        event.drivers.add(*drivers)
+
+
+        recipients_ids = data.get('recipients', [])
+        recipients = Recipient.objects.filter(id__in=recipients_ids)
+        event.recipients.add(*recipients)
+
+        return JsonResponse({'message': 'Event created successfully'}, status=201)
+
+    return JsonResponse({'error': 'Only POST method is allowed'}, status=405)
+
