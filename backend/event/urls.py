7ce86d871490e97ee1e71f22685ecaef0adf4db2
Josie Fessey <07jreckn@gmail.com>
2024-07-18 11:59:31 +0300

    first commit

---
+++ backend/event/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('events/update/<int:event_id>/', views.update_event, name='update_event'),
+    path('events/delete/<int:event_id>/', views.delete_event, name='delete_event'),
+
+]
