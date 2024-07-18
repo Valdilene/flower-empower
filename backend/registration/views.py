8830cbc83fde98c0be30dacfcbf7c06312237d6e
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-18 17:40:09 +0200

    backend-password-forget-reset: - linting

--- backend/registration/views.py
+++ backend/registration/views.py
@@ -64,7 +64,5 @@ class PasswordResetValidationView(GenericAPIView):
     def patch(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(
-            serializer.validated_data,
-        )
+        serializer.save(serializer.validated_data)
         return Response(status=status.HTTP_200_OK)
