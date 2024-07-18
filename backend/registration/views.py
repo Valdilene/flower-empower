9237cf382930896d3268ad334ead671daf63e59b
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-18 17:41:00 +0200

    backend-password-forget-reset: - linting

--- backend/registration/views.py
+++ backend/registration/views.py
@@ -3,10 +3,8 @@ from random import randint
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from rest_framework import status
-from rest_framework.exceptions import ValidationError
-from rest_framework.generics import GenericAPIView, CreateAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from registration.models import Registration
 from registration.serializers import RegistrationSerializer, RegistrationValidationSerializer, PasswordResetSerializer, \
