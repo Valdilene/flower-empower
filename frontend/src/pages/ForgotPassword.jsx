c2ae66f774cd1d20b1628d6f60198963fcbc67c4
Josie Fessey <07jreckn@gmail.com>
2024-07-19 11:59:54 +0300

    added deleted files

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -5,27 +5,25 @@ function ForgotPassword() {
   const { register, handleSubmit } = useForm();
 
   return (
-    <div className="flex h-full items-center justify-center">
-      <div className="flex h-2/4 flex-col items-center gap-14">
-        <h2 className=" text-3xl">FORGOT PASSWORD</h2>
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6">
-            <LabelAndInput
-              htmlFor="email"
-              type="text"
-              register={register}
-              name="email"
-            >
-              Email address
-            </LabelAndInput>
+    <div className="flex flex-col h-full items-center justify-start mt-40">
+      <h2 className=" text-3xl">FORGOT PASSWORD</h2>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="mt-10 grid w-full grid-cols-1  gap-x-4 space-y-6">
+          <LabelAndInput
+            htmlFor="email"
+            type="text"
+            register={register}
+            name="email"
+          >
+            Email address
+          </LabelAndInput>
 
-            <div>
-              <button className="flex w-full justify-center rounded-md bg-pink-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-600 ">
-                SEND CODE
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="col-span-2 place-self-center justify-self-center">
+            <button className=" rounded-md bg-pink-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-600 ">
+              SEND CODE
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
