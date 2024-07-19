3c8326043df2e1c3853e2eba406d8207f461fe4c
JayCox90 <jaycox@gmx.net>
2024-07-19 14:43:49 +0200

    fixed some stuff that got broke in merge request by someone who forgot to pull master first and a tech lead who didn't read through the changes... tztztz <3

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -6,7 +6,8 @@ from project import settings
 class Registration(models.Model):
     code = models.IntegerField(null=True, blank=True)
     email = models.EmailField(unique=True)
-    user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True,
+                                related_name='registration_profile')
 
     def __str__(self):
         return f"registration with code {self.code}"
