cad7eebbfbe2c02a09e1a570724dc09cda7d59f6
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-07-19 15:36:07 +0200

    Add admin user end points 2

--- backend/recipient/urls.py
+++ backend/recipient/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-
-from .views import ListCreateRecipientView, RetrieveUpdateDestroyRecipientView
-
-urlpatterns = [
-    path('', ListCreateRecipientView.as_view()),
-    path('<int:recipient_id>/', RetrieveUpdateDestroyRecipientView.as_view())
-    ]
+# from django.urls import path
+#
+# from .views import ListCreateRecipientView, RetrieveUpdateDestroyRecipientView
+#
+# urlpatterns = [
+#     path('', ListCreateRecipientView.as_view()),
+#     path('<int:recipient_id>/', RetrieveUpdateDestroyRecipientView.as_view())
+#     ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,23 +1,25 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
-from user.permissions import IsAuthenticatedOrAdminUser
 from user.serializers import UserSerializer
 
 User = get_user_model()
 
 
 class MeView(GenericAPIView):
+    # View for retrieving, updating, or deleting the authenticated user's information.
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticatedOrAdminUser]
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
+        # Return the currently authenticated user.
         return self.request.user
 
     def get(self, request, *args, **kwargs):
+        # Handle GET request to retrieve user information
         user = self.get_object()
         if user is not None:
             serializer = self.get_serializer(user)
@@ -26,6 +28,7 @@ class MeView(GenericAPIView):
             return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
 
     def patch(self, request, *args, **kwargs):
+        # Handle PATCH request to partially update user information.
         user = self.get_object()
         serializer = self.get_serializer(user, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -33,18 +36,21 @@ class MeView(GenericAPIView):
         return Response(serializer.data)
 
     def delete(self, request, *args, **kwargs):
+        # Handle DELETE request to delete the user.
         user = self.get_object()
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ListCreateUserView(ListCreateAPIView):
+    # View for listing all users or creating a new user.
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAdminUser]
 
 
 class RetrieveUpdateDestroyUserView(RetrieveUpdateDestroyAPIView):
+    # View for retrieving, updating, or deleting a specific user by ID.
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_url_kwarg = "user_id"
