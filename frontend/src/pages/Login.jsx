7f66105165bca6498ab66966e31f91e212e1782d
Emir Murati <emir_810@icloud.com>
2024-07-22 16:12:48 +0200

    changed cookies typo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,7 @@ import Loader from "../components/Loader";
 import { useCookies } from "react-cookie";
 
 function Login() {
-  const [setCookie] = useCookies(["user"]);
+  const [cookies, setCookie] = useCookies(["user"]);
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
 
