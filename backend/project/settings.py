81aefa29112e7921ab3f197253a09d7b88f2419d
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-07-23 10:22:37 +0200

    add lines to requirements and settings related to celery and redis

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -43,9 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'drf_yasg',
     'rest_framework',
     'corsheaders',
+    'sendmail.apps.SendmailConfig',
+    'django_celery_results',
+    'django_celery_beat',
 
     'user',
     'event',
