76591dc20c5f557b4ae0ba9d3bdfe804b26f2516
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-07-23 13:12:06 +0200

    Fix lint issues.

--- backend/event/views.py
+++ backend/event/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.utils.dateparse import parse_date
 
 from rest_framework import status, permissions
-from rest_framework.generics import GenericAPIView, ListAPIView, ListCreateAPIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
 from rest_framework.response import Response
 from recipient.models import Recipient
 from .models import Event
