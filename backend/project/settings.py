cba9a1743a7802e9e9b1bdaf443507585f2f14a9
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-25 16:46:58 +0200

    backend-recipients-lon-lat-api-call:
    - This brings full functionality to implement csv file to our database with upload through the admin panel
    - This brings new functionality to add a recipient while doing automated api call to get lon and lat for the address send for creating the new recipient

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'import_export',
+    'requests',
 
     'user',
     'event',
--- backend/recipient/serializers.py
+++ backend/recipient/serializers.py
@@ -1,8 +1,40 @@
 from rest_framework import serializers
 from .models import Recipient
+from json import dumps
+import requests
 
 
 class RecipientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipient
-        fields = '__all__'
+        fields = ['first_name', 'last_name', 'address', 'city', 'state', 'zip', 'end_date', 'group', 'lon', 'lat']
+
+    def create(self, validated_data):
+        address = validated_data.get('address')
+        city = validated_data.get('city')
+        state = validated_data.get('state')
+        zip = validated_data.get('zip')
+
+        address_string = str(f'{address}, {city}, {state}, {zip}')
+
+        api_response = self.api_call(address_string)
+
+        response_results = api_response['results']
+
+        result_object = response_results[0]
+
+        validated_data['lon'] = result_object['lon']
+        validated_data['lat'] = result_object['lat']
+
+        return super().create(validated_data)
+
+    def api_call(self, address_string):
+        apiKey = '33a37056031e4222b3e9b659704ea36f'
+        url = f'https://api.geoapify.com/v1/geocode/search?&apiKey={apiKey}&text={address_string}&format=json'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise serializers.ValidationError('API Call failed')
+
