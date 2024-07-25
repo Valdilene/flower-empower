5a31b66eed9f933daf12ba2b345b7016f3448dca
Emir Murati <emir_810@icloud.com>
2024-07-25 13:08:46 +0200

    event id page design fix

--- frontend/src/components/event/EventRow.jsx
+++ frontend/src/components/event/EventRow.jsx
@@ -1,5 +1,4 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { CiLock, CiUnlock } from "react-icons/ci";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
