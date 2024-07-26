4d979b8bbd4ac90614d8cff2e9922f34328e03f5
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-26 14:48:07 +0200

    backend-api-call-routes: - email sending setup to drivers - api call to get routes
    - small fix to get id with recipient for Emir

--- backend/event/views.py
+++ backend/event/views.py
@@ -1,5 +1,9 @@
+from json import dumps
+import requests
+import math
+
 from django.contrib.auth import get_user_model
-from django.core.mail import send_mass_mail
+from django.core.mail import send_mass_mail, send_mail
 from django.utils.dateparse import parse_date
 from rest_framework import status
 from rest_framework.generics import GenericAPIView, ListCreateAPIView
@@ -7,6 +11,7 @@ from rest_framework.response import Response
 from recipient.models import Recipient
 from rest_framework.views import APIView
 
+from user.models import User
 from .models import Event
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .serializers import EventSerializer, EventAdminSerializer
@@ -163,5 +168,125 @@ class SendBouquetMakersEmailView(APIView):
             for recipient in recipients
         ]
         send_mass_mail(messages)
+
+        return Response({'message': 'Emails sent.'}, status=status.HTTP_200_OK)
+
+
+class SendDriversEmailView(APIView):
+    queryset = Event.objects.all()
+    serializer_class = EventAdminSerializer
+    permission_classes = [IsAdminUser]
+    lookup_url_kwarg = "event_id"
+
+    def get_object(self):
+        # Override the get_object method to use the lookup_field
+        return Event.objects.get(pk=self.kwargs.get(self.lookup_url_kwarg))
+
+    def post(self, request, *args, **kwargs):
+        event = self.get_object()
+
+        drivers = list(User.objects.filter(id__in=event.drivers.all()))
+        group = event.group
+        recipients = list(Recipient.objects.filter(group=group))
+
+        subject = request.data.get('subject')
+        body = request.data.get('body')
+
+        list_of_locations = []
+
+        # build location part
+        for recipient in recipients:
+            location_object = {
+                "location": [
+                    recipient.lon, recipient.lat
+                ],
+                "duration": 300,
+                "delivery_amount": 1,
+                "description": str(
+                    f'{recipient.first_name} {recipient.last_name}, {recipient.address}, {recipient.city}, {recipient.state}, {recipient.zip}'),
+            }
+            list_of_locations.append(location_object)
+
+        # build final request object
+        delivery_capacity_float = len(recipients) / len(drivers)
+        delivery_capacity = math.ceil(delivery_capacity_float)
+        amount_of_drivers = len(drivers)
+        drivers_list = []
+        driver = {
+            "start_location": [
+                -119.708527, 34.424501
+            ],
+            "delivery_capacity": delivery_capacity
+        }
+
+        for drivers in range(amount_of_drivers):
+            drivers_list.append(driver)
+
+        final_request_object = {
+            "mode": "drive",
+            "agents": drivers_list,
+            "jobs": list_of_locations
+        }
+
+        apiKey = '33a37056031e4222b3e9b659704ea36f'
+        url = f'https://api.geoapify.com/v1/routeplanner?&apiKey={apiKey}'
+        data = dumps(final_request_object)
+        data = data.encode('utf-8')
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, data=data, headers=headers)
+        response = response.json()
+
+        # building lists for drivers with the response
+
+        # isolation of the features_list, which holds the actions. In these the mapping calculated by the API is stored
+
+        features_list = response['features']
+
+        # isolation the jobs_list, which holds the description value. In this the address from the Excel is stored
+
+        properties_object = response['properties']
+        params_object = properties_object['params']
+        jobs_list = params_object['jobs']
+
+        num = 0
+
+        routes_list = []
+
+        for item in features_list:
+            num += 1
+
+            route_list = []
+
+            # isolation of the actions_list which is the order of addresses to drive to
+
+            properties_object = item['properties']
+            time = int(properties_object['time'] / 60)
+            actions_list = properties_object['actions']
+
+            route_list.append(f'-----------DRIVER: {num}-------------APPROX. DRIVING TIME: {time} min-----------')
+
+            # iterating through the actions_list and find actions with a job_index. So a action,
+            # which is connected to an address in the jobs_list
+
+            for action in actions_list:
+                if 'job_index' in action.keys():
+                    # take the job_index in the action and use this job_index to find the job in the jobs_list.
+                    # In this job the description key holds the value with the address object from the Ex el at the beginning.
+
+                    address_object = jobs_list[action['job_index']]
+                    route_list.append(address_object['description'])
+
+            routes_list.append(route_list)
+
+        if len(routes_list) == 0 or not routes_list:
+            return Response({'message': 'Emails sent.'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+        for number_of_driver in range(len(routes_list)):
+            send_mail(
+                'Flower Empower – Your route for tomorrow',
+                'Here is your route for tomorrow: {}'.format(routes_list[number_of_driver]),
+                'flower.empower.management@gmail.com',
+                ['{}'.format(list(User.objects.filter(id__in=event.drivers.all()))[number_of_driver].email)],
+                fail_silently=False, )
 
         return Response({'message': 'Emails sent.'}, status=status.HTTP_200_OK)
