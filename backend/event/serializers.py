76702afa43b4f30a7fcbbfc5d4f862f5c227b389
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-07-26 16:13:51 +0200

    Implement stats for home page

--- backend/event/serializers.py
+++ backend/event/serializers.py
@@ -15,11 +15,15 @@ class EventSerializer(serializers.ModelSerializer):
 class EventAdminSerializer(serializers.ModelSerializer):
     drivers = UserSerializer(many=True, read_only=True)
     bouquet_makers = UserSerializer(many=True, read_only=True)
-    recipients = RecipientSerializer(many=True, read_only=True)
+    # recipients = RecipientSerializer(many=True, read_only=True)
+    recipients_count = serializers.SerializerMethodField()
+
+    def get_recipients_count(self, obj):
+        return obj.recipients.count()
 
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = ['id', 'drivers', 'bouquet_makers', 'recipients_count', 'date', 'bouquet_makers_needed', 'drivers_needed','closed']
 
 # def validate_date(self, value):
 #     parsed_date = parse_date(value)
