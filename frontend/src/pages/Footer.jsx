0a64a729791f76c70b3c659535689e2f4d02c06a
Pooja Kumbhar <pooja.p.kumbhar@gmail.com>
2024-07-29 17:06:20 +0200

    Add contact information

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -54,27 +54,80 @@ const navigation = {
 
 function Footer() {
   return (
-    <footer className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <div className="mt-10 flex justify-center space-x-10">
-          {navigation.social.map((item) => (
-            <a
-              target="_blank"
-              key={item.name}
-              href={item.href}
-              className="text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">{item.name}</span>
-              <item.icon aria-hidden="true" className="h-6 w-6" />
-            </a>
-          ))}
+      <footer className="bg-white ">
+        <div>
+          <h6
+              className="mb-5 flex justify-center font-semibold uppercase md:justify-start ">
+            Contact
+          </h6>
+          <div className="flex flex-row items-center md:items-start space-y-4 mb-2">
+            <p className="flex items-center mr-5 mt-4 text-gray-500">
+    <span className="mr-5  [&>svg]:h-5 [&>svg]:w-5">
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor">
+        <path
+            d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"/>
+        <path
+            d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"/>
+      </svg>
+    </span>
+              1528 Chapala Street, Suite 304, Santa Barbara, CA 93101
+            </p>
+            <p className="flex items-center mr-5 text-gray-500">
+    <span className="mr-3 [&>svg]:h-5 [&>svg]:w-5 ml-5">
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor">
+        <path
+            d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z"/>
+        <path
+            d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z"/>
+      </svg>
+    </span>
+              flowerempower@dreamfoundation.org
+            </p>
+            <p className="flex items-center mr-3 text-gray-500">
+    <span className="mr-3 [&>svg]:h-5 [&>svg]:w-5 ml-5 ">
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor">
+        <path
+            fillRule="evenodd"
+            d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+            clipRule="evenodd"/>
+      </svg>
+    </span>
+              888-437-3267
+            </p>
+          </div>
+
         </div>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; Proudly built and maintained by Flower Empower Team © 2024 |
-          All Rights Reserved
-        </p>
-      </div>
-    </footer>
+
+        <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+          <div className="mt-10 flex justify-center space-x-10">
+            {navigation.social.map((item) => (
+                <a
+                    target="_blank"
+                    key={item.name}
+                    href={item.href}
+                    className="text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">{item.name}</span>
+                  <item.icon aria-hidden="true" className="h-6 w-6"/>
+                </a>
+            ))}
+          </div>
+          <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+            &copy; Proudly built and maintained by Flower Empower Team © 2024 |
+            All Rights Reserved
+          </p>
+        </div>
+
+      </footer>
   );
 }
 
