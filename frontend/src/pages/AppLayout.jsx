39a9dca57a5775902862bcda3825c4635fe4f852
Pooja Kumbhar <pooja.p.kumbhar@gmail.com>
2024-07-31 17:47:33 +0200

    Change the dropdown to logout

--- frontend/src/pages/AppLayout.jsx
+++ frontend/src/pages/AppLayout.jsx
@@ -80,36 +80,10 @@ function AppLayout() {
                 <div className="ml-4 flex items-center md:ml-6">
                   {/* Profile dropdown */}
                   {token ? (
-                    <Menu as="div" className="relative ml-3">
-                      <div>
-                        <MenuButton className="relative flex max-w-xs items-center rounded-full bg-[#e48dde] text-sm ">
-                          <span className="absolute -inset-1.5" />
-                          <span className="sr-only">Open user menu</span>
-                          <img
-                            alt=""
-                            src={user.imageUrl}
-                            className="h-8 w-8 rounded-full"
-                          />
-                        </MenuButton>
-                      </div>
-                      <MenuItems
-                        transition
-                        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                      >
-                        {token ? (
-                          <>
-                            <MenuItem>
-                              <button
-                                onClick={() => handleLogout()}
-                                className="block w-full text-start px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                              >
-                                Logout
-                              </button>
-                            </MenuItem>
-                          </>
-                        ) : null}
-                      </MenuItems>
-                    </Menu>
+
+                    <NavLink
+                              to="/login" onClick={() => handleLogout()}
+                              className="text-slate-500 hover:text-slate-800 hover:border-b-2 hover:border-b-slate-400  py-2 px-4">Logout</NavLink>
                   ) : null}
                 </div>
               </div>
