74a7fec319f2254eb002ca0c070b521411d4436e
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-07-31 16:00:48 +0200

    Add ToggleEventAttendanceView. Add 2 fields to event: bouquet makers attendees and drivers attendees.

--- backend/event/serializers.py
+++ backend/event/serializers.py
@@ -13,6 +13,7 @@ class EventSerializer(serializers.ModelSerializer):
 class EventAdminSerializer(serializers.ModelSerializer):
     drivers = UserSerializer(many=True, read_only=True)
     bouquet_makers = UserSerializer(many=True, read_only=True)
+
     recipients_count = serializers.SerializerMethodField()
 
     def get_recipients_count(self, obj):
@@ -20,4 +21,5 @@ class EventAdminSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ['id', 'drivers', 'bouquet_makers', 'group', 'recipients_count', 'date', 'bouquet_makers_needed', 'drivers_needed', 'closed']
+        fields = ['id', 'drivers', 'bouquet_makers', 'group', 'recipients_count', 'date', 'bouquet_makers_needed',
+                  'drivers_needed', 'bouquet_makers_attendees', 'drivers_attendees', 'closed']
